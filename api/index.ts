527cd69202b2fa22aca4067ba3d5044db58a7215
Gajaananth <gajaananthnadan17898@gmail.com>
2026-03-05 01:03:03 +0530

    v1.7.7: ESM-ISO Mode (Static Import Bundling, API Module Boundary)

---
+++ api/index.ts
@@ -0,0 +1,8 @@
+import app from '../server/src/index';
+
+/**
+ * Zium Nova Entry Layer v1.7.7 (ESM-ISO MODE)
+ * 🦅🛰️ This file is statically compiled by Vercel to ensure 
+ * 100% of server code is bundled into the lambda environment.
+ */
+export default app;
